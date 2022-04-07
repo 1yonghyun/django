3f1c8538c74c243801d41202b101da170e5b4f98
1yonghyun <zyonghyunz@gmail.com>
2022-04-07 17:22:38 +0900

    index

--- 0406/articles/views.py
+++ 0406/articles/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
+
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
